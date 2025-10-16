89870f1a209a9cf635c4c3bea7ee7a3a48d8673b
Jolly <pop24238@gmail.com>
2025-10-16 15:57:27 -0400

    boop

---
+++ src/JavaBeginnerExercises.java
@@ -0,0 +1,18 @@
+public class JavaBeginnerExercises {
+    public static void main(String[] args) {
+
+        // ========================================
+        // HELLO WORLD EXERCISES
+        // ========================================
+
+        // Exercise 1: Print your name
+        // TODO: Print "My name is [YourName]"
+        System.out.println("My name is Jolly");
+
+        // Exercise 2: Print multiple lines
+        // TODO: Print 3 lines: your name, your age, and one other thing. Go wild.
+        System.out.println("My name is Jolly.");
+        System.out.println("I'm 28? I think");
+        System.out.println("Mkheimerjenkensberger.");
+    }
+}
